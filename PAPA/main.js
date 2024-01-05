ca39308c0a42b48bdee69ec093f23970617c4d81
kevinpepe <155782101+kevinpepe@users.noreply.github.com>
2024-01-05 16:45:24 -0600

    Add files via upload

---
+++ PAPA/main.js
@@ -0,0 +1,10 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const button = document.getElementById('button');
+    
+
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+ 
